28688b761c76941cd02d694fb0561f33ad179fda
Una LEstrange <lestrange.una@itsligo.ie>
2021-03-02 21:13:01 +0000

    Add project files.

---
+++ SampleForCA2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SampleForCA2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] namesEntered = new string[4];
+            int[] agesEntered = new int[4];
+
+
+            // write code which asks the user to input
+            // a name and an age for 4 people.
+
+            //it then calculates the minimum, average and max age
+            // and writes these out to the console.
+
+            // when taking in an age your code should check that the age is valid
+            // i.e. it is an int >= 0
+            // and if it isn't prompt for a new one, until the user enters a valid
+            // age.
+
+
+        }
+
+
+
+        
+    }
+}
